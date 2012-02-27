978644ae5c581094d8e8491a927eb7af37115535
charles marshall <charles@oneblackbear.com>
2012-02-27 14:42:11 +0000

    add in the hidden module to get assets loaded in the cms

--- setup.php
+++ setup.php
@@ -1,3 +1,4 @@
 <?
+CMSApplication::register_module("media.youtube", array("hidden"=>true, "plugin_name"=>"wildfire.media.youtube", 'assets_for_cms'=>true));
 WildfireMedia::$classes[] = 'WildfireYoutubeFile';
 ?>
