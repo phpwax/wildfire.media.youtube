62761148b92c049c2b599eaab8f361d8d7639538
mikenosko <michal@oneblackbear.com>
2013-07-03 12:00:01 +0100

    youtube hover-info.js tweak - working of the rendered imgs data

--- resources/public/javascripts/wildfire.media.youtube/hover-info.js
+++ resources/public/javascripts/wildfire.media.youtube/hover-info.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function(){
 
   jQuery(window).bind("media.wildfireyoutubefile.preview", function(e, row, preview_container){
     var yt_iframe = jQuery("<iframe>"),
-        permalink = row.find("img").data("permalink")+'?rel=0&wmode=opaque&controls=0';
+        data = row.find("img").data(),
+        html = '';
 
-    yt_iframe.attr({"src":permalink,"width":200,"height":Math.floor(200/1.778),"frameborder":0,"allowfullscreen":true});
-
-    preview_container.html(yt_iframe);
+    html = yt_iframe.attr(data).wrap("<div>").parent().html();
+    preview_container.html(html);
 
   });
 
