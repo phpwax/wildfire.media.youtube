5e54cc65baefefeda7cb829a6260944a09dc3568
mikenosko <michal@oneblackbear.com>
2013-07-22 17:27:04 +0100

    registering assets

--- ini.php
+++ ini.php
@@ -1,5 +1,7 @@
 <?
-AutoLoader::register_assets("javascripts/wildfire.media.youtube",__DIR__."/resources/public/javascripts/wildfire.media.youtube/", "/*.js");
+AutoLoader::register_assets("javascripts/wildfire.media.youtube",__DIR__."/resources/public/javascripts/wildfire.media.youtube", "/*.js");
+AutoLoader::register_assets("stylesheets/wildfire.media.youtube",__DIR__."/resources/public/stylesheets/wildfire.media.youtube", "/*.css");
+AutoLoader::register_assets("images/wildfire.media.youtube",__DIR__."/resources/public/images/wildfire.media.youtube", "/*.png");
 AutoLoader::$plugin_array[] = array("name"=>"wildfire.media.youtube","dir"=>__DIR__);
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
--- setup.php
+++ setup.php
@@ -1,5 +1,7 @@
 <?
 CMSApplication::register_module("media.youtube", array("hidden"=>true, "plugin_name"=>"wildfire.media.youtube"));
 CMSApplication::register_asset("wildfire", "js", "wildfire.media.youtube");
+CMSApplication::register_asset("wildfire", "css", "wildfire.media.youtube");
+
 WildfireMedia::$classes[] = 'WildfireYoutubeFile';
 ?>
